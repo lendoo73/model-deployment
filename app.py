27fd36449e7afe8c4028165290cc10965c8ac9c1
Csaba Cselko <ccselko@icomera.onmicrosoft.com>
2024-01-13 16:15:06 +0000

    api endpoint created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 tokenizer = pickle.load(open("models/cv.pkl", "rb"))
@@ -14,12 +14,23 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    if request.method == "POST":
-        email_text = [request.form.get("content")]
-    tokenized_email = tokenizer.transform(email_text) # this is the preprocessed text
-    predictions = model.predict(tokenized_email)
-    predictions = 1 if predictions == 1 else -1
-    return render_template("index.html", predictions=predictions, email_text=email_text[0])
+    email = [request.form.get("content")]
+    tokenized_email = tokenizer.transform(email) # this is the preprocessed text
+    prediction = model.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return render_template("index.html", prediction=prediction, email_text=email[0])
+
+
+@app.route("/api/predict", methods=["POST"])
+def api_predict():
+    data = request.get_json(force=True)
+    email = data["content"]
+    if type(email) != list:
+        email = [email]
+    tokenized_email = tokenizer.transform(email)  # this is the preprocessed text
+    prediction = model.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({"prediction": prediction})
 
 
 if __name__ == "__main__":
